e449fb865c325c6bced16147a2e53767325502b4
jesusf650 <73547682+jesusf650@users.noreply.github.com>
2020-10-31 15:24:49 -0500

    Update proyecto1.1.py

--- proyecto1.1.py
+++ proyecto1.1.py
@@ -1,1 +1,7 @@
-#sdfsfdfafaf
+# Calculadora de Matrices
+from Metodos_Calculadora import *
+# Menú:
+menu = int(input('Seleccionar opción: '))
+if menu in [1, 2]:
+    lmatrices = []
+    lmatrices.IngresarMatrices() if menu == 1 else lmatrices.OperarMatrices()
