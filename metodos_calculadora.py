a99629790bda9d965db4b473e3a78ecbbe53af89
jesusf650 <73547682+jesusf650@users.noreply.github.com>
2020-11-04 13:12:15 -0500

    Update metodos_calculadora.py

--- metodos_calculadora.py
+++ metodos_calculadora.py
@@ -1,1 +1,21 @@
-
+# Función Mostrar Matrices
+def MostrarMatrices():
+  print('[1]Listar Matrices\n[2]Escoger Matriz\n[3]Finalizar')
+  opcion = int(input('Mostrar Matriz - Seleccionar opción: '))
+  if opcion == 1:
+    for i, matriz in enumerate(lmatrices):
+      print(f'Matriz {i+1}: {len(matriz)}x{len(matriz[0])}')
+  elif opcion == 2:
+    for i, matriz in enumerate(lmatrices):
+      print(f'Matriz {i+1}: {len(matriz)}x{len(matriz[0])}')
+    matriz = int(input('Ingrese numero de Matriz: '))-1
+    for f, fila in enumerate(lmatrices[matriz]):
+      for c, columna in enumerate(fila):
+        print(' ', lmatrices[matriz][f][c], end='')
+      print()
+      
+# Función Finalizar
+def Finalizar():
+  global menu
+  menu = 4
+  print('Proceso finalizado')
