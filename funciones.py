eed1692ab08ec7de89724dff35220260be58e99d
jesusf650 <73547682+jesusf650@users.noreply.github.com>
2020-12-22 00:10:05 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,7 +1,8 @@
 from menus import *
+
 # Función Ingresar Matrices
 def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
-    memoria = open('memoria.txt', 'w')
+    memoria = open('memoria.txt', 'w+')
     while True:
         indice = 0
         if (opcion := MenuIngresarMatrices()) == '1':
@@ -11,9 +12,10 @@ def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
                 if confirmacion == '1':
                     for i, matriz in enumerate(lmatrices[indiceM]):
                         print(f'Matriz {i + 1}: {len(matriz)}x{len(matriz[0])}')
-                    while not(indice := int(input("Reemplazar matriz - Ingrese índice: ")) in [i for i in range(1, len(lmatrices[indiceM])+1)]):
+                    while not (indice := int(input("Reemplazar matriz - Ingrese índice: ")) in [i for i in range(1, len(
+                            lmatrices[indiceM]) + 1)]):
                         print('Indice Inexistente. Intente nuevamente')
-                    lmatrices[indiceM].remove(lmatrices[indiceM][indice-1])
+                    lmatrices[indiceM].remove(lmatrices[indiceM][indice - 1])
                     memoria.write(str(lmatrices))
                     break
                 elif confirmacion == '2':
@@ -26,10 +28,26 @@ def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
                 if len(new_matriz[0]) != len(f):
                     print("¡Vaya! parece que el numero de columnas no coincide porfavor inserte una matriz valida")
                     new_matriz = None
+            laberinto = True
+            for i in range(len(new_matriz)):
+                for j in range(len(new_matriz[i])):
+                    if not(j in [0, 1]):
+                        laberinto = False
             if new_matriz:
-                lmatrices[indiceM].append(new_matriz) if opcion == '2' else lmatrices[indiceM].insert(indice-1, new_matriz)
+                lmatrices[indiceM].append(new_matriz) if opcion == '2' else lmatrices[indiceM].insert(indice - 1, new_matriz)
                 memoria.write(str(lmatrices))
-                print("La matriz se guardo exitosamente ")
+                if not laberinto:
+                    while True:
+                        print("El laberinto se guardó exitosamente. ¿Desea resolver el laberinto?\n[1]Si\n[2]No")
+                        confirmacion_lab = input("Resolver laberinto - Seleccionar opción: ")
+                        if confirmacion_lab == '1':
+                            solve_laberinto(new_matriz)
+                            break
+                        elif confirmacion_lab == '2':
+                            break
+                        print('Opción Inexistente. Intente nuevamente')
+                else:
+                    print("La matriz se guardó exitosamente ")
         else:
             if guardar:
                 lmatrices.append(new_matriz) if opcion == '2' else lmatrices.insert(indice - 1, new_matriz)
@@ -48,18 +66,20 @@ def OperarMatrices(lmatrices, indiceM):
                 for i, matriz in enumerate(lmatrices[indiceM]):
                     print(f'Matriz {i + 1}: {len(matriz)}x{len(matriz[0])}')
                 indices = [i for i in range(len(lmatrices[indiceM]))]
-                while not ((indice1 := int(input("Operar matriz - Ingrese índice 1: "))-1) in indices):
+                while not ((indice1 := int(input("Operar matriz - Ingrese índice 1: ")) - 1) in indices):
                     print('Indice Inexistente. Intente nuevamente')
                 indice2 = indice1
                 if operacion != '4':
-                    while not ((indice2 := int(input("Operar matriz - Ingrese índice 2: "))-1) in indices):
+                    while not ((indice2 := int(input("Operar matriz - Ingrese índice 2: ")) - 1) in indices):
                         print('Indice Inexistente. Intente nuevamente')
                 if len(matriz1 := lmatrices[indiceM][indice1]) == len(matriz2 := lmatrices[indiceM][indice2]):
                     matriz_res = []
                     if operacion == '1':
-                        matriz_res = [[matriz1[f][c]+matriz2[f][c] for c in range(len(matriz1))] for f in range(len(matriz1))]
+                        matriz_res = [[matriz1[f][c] + matriz2[f][c] for c in range(len(matriz1))] for f in
+                                      range(len(matriz1))]
                     elif operacion == '2':
-                        matriz_res = [[matriz1[f][c]-matriz2[f][c] for c in range(len(matriz1))] for f in range(len(matriz1))]
+                        matriz_res = [[matriz1[f][c] - matriz2[f][c] for c in range(len(matriz1))] for f in
+                                      range(len(matriz1))]
                     elif operacion == '3':
                         temp = 0
                         for f in range(len(matriz1)):
@@ -77,7 +97,8 @@ def OperarMatrices(lmatrices, indiceM):
                             print()
                         IngresarMatrices(lmatrices, indiceM, True, matriz_res)
                 else:
-                    print("¡Vaya! parece que las dimensiones de ambas matrices no coincide. Seleccione dos matrices válidas")
+                    print(
+                        "¡Vaya! parece que las dimensiones de ambas matrices no coincide. Seleccione dos matrices válidas")
     else:
         print("¡Vaya! parece que aún no hay matrices guardadas. Ingrese 2 o más matrices")
         IngresarMatrices(lmatrices, indiceM)
@@ -109,63 +130,55 @@ def Finalizar():
     print('Proceso finalizado! Gracias')
     sys.exit()
 
+
 # Laberinto
 def solve_laberinto(laberinto):
-    #Tamaño_del_laberinto
+    # Tamaño_del_laberinto
     N = len(laberinto)
     M = len(laberinto[0])
     def is_safe(laberinto, x, y):
-        #Funcion utilitaria que verifica si x, y son indices validos
-        if x >= 0 and y >= 0 and x < N and y < M and laberinto[x][y] == 1 and sol[x][y] != 1:
+        # Funcion utilitaria que verifica si x, y son indices validos
+        if 0 <= x < N and 0 <= y < M and laberinto[x][y] == 1 and sol[x][y] != 1:
             return True
         return False
     def print_solution(sol):
-    #Funcion utilitaria para imprimir la solución de la matriz
+        # Funcion utilitaria para imprimir la solución de la matriz
         for i in sol:
             for j in i:
-                print(str(j) + " ", end = "")
+                print(str(j) + " ", end="")
             print("")
-    #Función Recursiva utilitaria para resolver el problema del laberinto
+    # Función Recursiva utilitaria para resolver el problema del laberinto
     def solve_laberinto_util(laberinto, x, y, sol):
         # if (x, y is goal) return True
-        if x == N-1 and y == M-1:
+        if x == N - 1 and y == M - 1:
             sol[x][y] = 1
             return True
-        #verificar si laberinto[x][y] es valido
-        if is_safe(laberinto, x, y) == True:
-            #marcar x, y como parte de la solución
+        # verificar si laberinto[x][y] es valido
+        if is_safe(laberinto, x, y):
+            # marcar x, y como parte de la solución
             sol[x][y] = 1
-            #movemos hacia adelante
-            if solve_laberinto_util(laberinto, x+1, y, sol) == True:
+            # movemos hacia adelante
+            if solve_laberinto_util(laberinto, x + 1, y, sol):
                 return True
-            #si moviendo hacia adelante no nos da la solución
+            # si moviendo hacia adelante no nos da la solución
             # #entonces movemos hacia abajo
-            if solve_laberinto_util(laberinto, x, y+1, sol) == True:
+            if solve_laberinto_util(laberinto, x, y + 1, sol):
                 return True
             # hacia atras
-            if y >= 1  and solve_laberinto_util(laberinto, x, y-1, sol) == True:
+            if y >= 1 and solve_laberinto_util(laberinto, x, y - 1, sol):
                 return True
             # hacia arriba
-            if x >= 1 and solve_laberinto_util(laberinto, x-1, y , sol) == True:
+            if x >= 1 and solve_laberinto_util(laberinto, x - 1, y, sol):
                 return True
-
-            #si ninguno de los movimientos funciona
+            # si ninguno de los movimientos funciona
             # #BACKTRACK: desmarcamos x, y como parte de la solución
             sol[x][y] = 0
             return False
         return False
     # Crear a NxN 2D list
     sol = [[0 for i in range(M)] for j in range(N)]
-    if solve_laberinto_util(laberinto, 0, 0, sol) == False:
+    if not solve_laberinto_util(laberinto, 0, 0, sol):
         print("No existe Solución")
         return False
     print_solution(sol)
     return True
-Laberinto = [[1,1,1,1,1,1,0,1,1,1],
-             [0,0,0,0,0,1,1,1,0,0],
-             [1,1,1,1,0,1,0,1,0,0],
-             [1,0,0,1,1,1,0,1,1,1],
-             [1,0,0,0,0,0,0,0,0,0],
-             [1,1,1,1,1,1,1,1,1,1]]
-
-solve_laberinto(Laberinto)
