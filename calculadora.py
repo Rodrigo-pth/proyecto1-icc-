0bdf68bf77e0faccb10be43e5bdfde0e08f39a31
Rodrigo-pth <rodrigo.villafuerte@utec.edu.pe>
2020-12-22 13:06:11 +0100

    temino n n

--- calculadora.py
+++ calculadora.py
@@ -4,18 +4,23 @@ import json
 
 # Iniciar Sesión
 print("BIENVENIDO A TU CALCULADORA DE MATRICES FAVORITA!")
-users = ["Administrador", "Invitado"]
-memoria = open('memoria.txt', 'r')
+memoria = open('memoria.txt', 'r+')
+memoria_users = open('memoria_users', 'r+')
 lmatrices = json.loads(memoria.read())
+users = json.loads(memoria_users.read())
+memoria.close()
+memoria_users.close()
 if (user := input("Ingrese su usuario: ")) in users:
     indiceM = users.index(user)
 else:
     indiceM = len(users)
     users.append(user)
     lmatrices.append([])
-
 # Menú Principal
+
 while True:
+    memoria = open('memoria.txt', 'r+')
+    memoria_users = open('memoria_users', 'r+')
     print('[1]Ingresar Matrices\n'
           '[2]Operar Matrices\n'
           '[3]Mostrar Matrices\n'
@@ -23,8 +28,12 @@ while True:
     menu = input('Menú - Seleccionar opción: ')
     if menu in '12':
         IngresarMatrices(lmatrices, indiceM) if menu == '1' else OperarMatrices(lmatrices, indiceM)
-        memoria.close()
+
     elif menu in '34':
-        MostrarMatrices(lmatrices, indiceM) if menu == '3' else Finalizar()
+        MostrarMatrices(lmatrices, indiceM) if menu == '3' else Finalizar(lmatrices,users)
     else:
         print('Opción Inexistente. Intente nuevamente')
+    json.dump(lmatrices,memoria)
+    json.dump(users,memoria_users)
+    memoria.close()
+    memoria_users.close()
--- funciones.py
+++ funciones.py
@@ -1,11 +1,13 @@
 from menus import *
+import json
 
 # Función Ingresar Matrices
 def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
-    memoria = open('memoria.txt', 'w+')
     while True:
+        confirmacion = 0
         indice = 0
-        if (opcion := MenuIngresarMatrices()) == '1':
+        opcion = MenuIngresarMatrices()
+        if opcion == '1':
             while True:
                 print('¿Esta segur@ que desea reemplazar una memoria existente?\n[1]Si\n[2]No')
                 confirmacion = input("Reemplazar matriz - Seleccionar opción: ")
@@ -16,11 +18,12 @@ def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
                             lmatrices[indiceM]) + 1)]):
                         print('Indice Inexistente. Intente nuevamente')
                     lmatrices[indiceM].remove(lmatrices[indiceM][indice - 1])
-                    memoria.write(str(lmatrices))
                     break
                 elif confirmacion == '2':
                     break
                 print('Opción Inexistente. Intente nuevamente')
+        if confirmacion == '2':
+            break
         if opcion in '12' and not guardar:
             new_matriz = [[int(j) for j in input(f"Ingresar Matriz - Elementos de fila {i + 1}: ").split(",")]
                           for i in range(int(input("Ingresar Matriz - Número de filas: ")))]
@@ -28,14 +31,14 @@ def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
                 if len(new_matriz[0]) != len(f):
                     print("¡Vaya! parece que el numero de columnas no coincide porfavor inserte una matriz valida")
                     new_matriz = None
-            laberinto = True
+            laberinto = False
             for i in range(len(new_matriz)):
+
                 for j in range(len(new_matriz[i])):
-                    if not(j in [0, 1]):
-                        laberinto = False
+                    if not(new_matriz[i][j]  in [0, 1]):
+                        laberinto = True
             if new_matriz:
                 lmatrices[indiceM].append(new_matriz) if opcion == '2' else lmatrices[indiceM].insert(indice - 1, new_matriz)
-                memoria.write(str(lmatrices))
                 if not laberinto:
                     while True:
                         print("El laberinto se guardó exitosamente. ¿Desea resolver el laberinto?\n[1]Si\n[2]No")
@@ -49,12 +52,11 @@ def IngresarMatrices(lmatrices, indiceM, guardar=False, new_matriz=0):
                 else:
                     print("La matriz se guardó exitosamente ")
         else:
+            if opcion == '3':
+                break
             if guardar:
                 lmatrices.append(new_matriz) if opcion == '2' else lmatrices.insert(indice - 1, new_matriz)
-                memoria.write(str(lmatrices))
-                print("La matriz se guardó exitosamente ")
-                break
-            if opcion == '3':
+                print("salio ")
                 break
             print('Opción Inexistente. Intente nuevamente')
 
@@ -72,33 +74,38 @@ def OperarMatrices(lmatrices, indiceM):
                 if operacion != '4':
                     while not ((indice2 := int(input("Operar matriz - Ingrese índice 2: ")) - 1) in indices):
                         print('Indice Inexistente. Intente nuevamente')
+                matriz_res = []
                 if len(matriz1 := lmatrices[indiceM][indice1]) == len(matriz2 := lmatrices[indiceM][indice2]):
-                    matriz_res = []
                     if operacion == '1':
                         matriz_res = [[matriz1[f][c] + matriz2[f][c] for c in range(len(matriz1))] for f in
                                       range(len(matriz1))]
                     elif operacion == '2':
                         matriz_res = [[matriz1[f][c] - matriz2[f][c] for c in range(len(matriz1))] for f in
                                       range(len(matriz1))]
-                    elif operacion == '3':
-                        temp = 0
-                        for f in range(len(matriz1)):
-                            for c in range(len(matriz1)):
-                                for i in range(len(matriz1)):
-                                    temp += matriz1[f][i] * matriz2[i][c]
-                                matriz_res[f][c] = temp
-                    elif operacion == '4':
-                        matriz_res = [[matriz1[c][f] for c in range(len(matriz1))] for f in range(len(matriz1))]
-                    if not (matriz_res == []):
-                        print("Las matrices se operaron exitosamente ")
-                        for f, fila in enumerate(matriz_res):
-                            for c, columna in enumerate(fila):
-                                print(' ', matriz_res[f][c], end='')
-                            print()
-                        IngresarMatrices(lmatrices, indiceM, True, matriz_res)
+                if operacion == '3' and len(matriz1[0]) == len(matriz2):
+                    for i in range(len(matriz1)):
+                        matriz_res.append([])
+                    for f in range(len(matriz1)):
+                        for c in range(len(matriz2[f])):
+                            temp = 0
+                            for i in range(len(matriz1)):
+                                temp += matriz1[f][i] * matriz2[i][c]
+                            matriz_res[f].append(temp)
+                elif operacion == '4':
+                    matriz_res = [[matriz1[c][f] for c in range(len(matriz1))] for f in range(len(matriz1))]
+                if not (matriz_res == []):
+                    print("Las matrices se operaron exitosamente ")
+                    for f, fila in enumerate(matriz_res):
+                        for c, columna in enumerate(fila):
+                            print(' ', matriz_res[f][c], end='')
+                        print()
+                    IngresarMatrices(lmatrices, indiceM, True, matriz_res)
                 else:
                     print(
                         "¡Vaya! parece que las dimensiones de ambas matrices no coincide. Seleccione dos matrices válidas")
+                if (operacion := MenuOperarMatrices()) == '5':
+                    break
+
     else:
         print("¡Vaya! parece que aún no hay matrices guardadas. Ingrese 2 o más matrices")
         IngresarMatrices(lmatrices, indiceM)
@@ -125,8 +132,14 @@ def MostrarMatrices(lmatrices, indiceM):
         IngresarMatrices(lmatrices[indiceM])
 
 # Función Finalizar
-def Finalizar():
+def Finalizar(lmatrices,users):
     import sys
+    memoria = open('memoria.txt', 'r+')
+    memoria_users = open('memoria_users', 'r+')
+    json.dump(lmatrices, memoria)
+    json.dump(users, memoria_users)
+    memoria.close()
+    memoria_users.close()
     print('Proceso finalizado! Gracias')
     sys.exit()
 
@@ -175,7 +188,7 @@ def solve_laberinto(laberinto):
             sol[x][y] = 0
             return False
         return False
-    # Crear a NxN 2D list
+
     sol = [[0 for i in range(M)] for j in range(N)]
     if not solve_laberinto_util(laberinto, 0, 0, sol):
         print("No existe Solución")
